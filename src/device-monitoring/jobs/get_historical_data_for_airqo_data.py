62cf73d7c794afcdeb276a45c18151b5e2d30f75
Sserunjogi Richard <sserurich@gmail.com>
2020-09-13 11:43:24 +0300

    getting all channel ids from netmanagerdb

--- src/device-monitoring/jobs/get_historical_data_for_airqo_data.py
+++ src/device-monitoring/jobs/get_historical_data_for_airqo_data.py
@@ -1,48 +1,50 @@
 import pandas as pd
-from helpers import db_helpers
+from pymongo import MongoClient
 
-def get_agg_channel_data(channel_id,entire_dataset,freq='1H'):
-  '''
-  Get Hourly Aggregates using Mean of the Data for specified channel.
-  '''
+#MONGO_URI = os.getenv('MONGO_URI')
 
-  channel_data = entire_dataset[entire_dataset['channel_id'] == channel_id]
-  channel_data = channel_data.sort_values(by = 'time')[['time', 's1_pm2_5', 's1_pm10', 's2_pm2_5','s2_pm10', 'battery_voltage']]
-  channel_data['s1_s2_average_pm2_5'] = channel_data[['s1_pm2_5', 's2_pm2_5']].mean(axis=1).round(2)
-  channel_data['s1_s2_average_pm10'] = channel_data[['s1_pm10', 's2_pm10']].mean(axis=1).round(2)
+MONGO_URI = "mongodb://admin:airqo-250220-master@35.224.67.244:27017"
 
-  time_indexed_channel_data = channel_data.set_index('time')
-  #channel_data = channel_data.interpolate('time', limit_direction='both')
+from pymongo import MongoClient
 
-  channel_aggregated_data = time_indexed_channel_data.resample(freq).mean().round(2)
-  channel_aggregated_data['channel_id'] = channel_id
+def connect_mongo():
+    client = MongoClient(MONGO_URI)
+    db = client['airqo_netmanager']
+    return db
 
-  return channel_aggregated_data
+def get_hourly_aggregated_channel_data(channel_id,entire_dataset,freq='H'):
+    '''
+    Get Hourly Aggregates using Mean of the Data for specified channel.
+    '''
+   
+    channel_data = entire_dataset[entire_dataset['channel_id'] == channel_id]
+    channel_data['created_at'] = pd.to_datetime(channel_data['created_at'])
+    channel_data = channel_data.sort_values(by = 'created_at')[['created_at', 'pm2_5', 'pm10', 's2_pm2_5','s2_pm10', 'voltage']]
+    channel_data['s1_s2_average_pm2_5'] = channel_data[['pm2_5', 's2_pm2_5']].mean(axis=1).round(2)
+    channel_data['s1_s2_average_pm10'] = channel_data[['pm10', 's2_pm10']].mean(axis=1).round(2)
+
+    time_indexed_channel_data = channel_data.set_index('created_at')
+    #channel_data = channel_data.interpolate('time', limit_direction='both')
+
+    channel_aggregated_data = time_indexed_channel_data.resample('H').mean().round(2)
+    channel_aggregated_data['channel_id'] = channel_id
+
+    return channel_aggregated_data
+
 
 def get_hourly_channel_data(filename):
     #channel_id = 
 
     df = pd.read_csv(filename)
+    ## get the list of all the channels we have 
+    all_channel_ids = get_all_device_channels()
+    for channel in all_channel_ids:
+        channel_id = channel['channelID']
+         ## get data for each channel
+        channel_hourly_data = get_hourly_aggregated_channel_data(channel_id, df)
+        ## call a function that does the mapping
 
-    df['time'] = pd.to_datetime(df['time'])
-    df['time'] = df['time']
-    df['s1_pm2_5'] = pd.to_numeric(df['s1_pm2_5'], errors='coerce')
-    df['channel_id'] = pd.to_numeric(df['channel_id'], errors='coerce')
-    df['s1_pm10'] = pd.to_numeric(df['s1_pm10'], errors='coerce')
-    df['s2_pm2_5'] = pd.to_numeric(df['s2_pm2_5'], errors='coerce')
-    df['s2_pm10'] = pd.to_numeric(df['s2_pm10'], errors='coerce')
-    df['s1_s2_average_pm2_5'] = df[[
-        's1_pm2_5', 's2_pm2_5']].mean(axis=1).round(2)
-    df['s1_s2_average_pm10'] = df[['s1_pm10', 's2_pm10']].mean(axis=1).round(2)
-    df['battery_voltage'] = pd.to_numeric(df['battery_voltage'], errors='coerce')
-    time_indexed_data = df.set_index('time')
-    final_hourly_data = time_indexed_data.resample('H').mean().round(2)
-
-    ## get data for each channel e.g and loop through the data and call a function that 
-    ## does the mapping
-
-
-    ## save values for each channel to db
+        ## save values for each channel to db
 
     return 
 
@@ -54,28 +56,43 @@ def map_bigquery_data_to_netmanager_events_collection(channel_data, channel_id):
         #do the mapping and call the api for saving component values
         channel_name_value = channel_name['value']
         BASE_API_URL='http://127.0.0.1:3000/api/v1/devices/components/add/values?device='+channel_name_value
-        request_body = 
+       
+        ''' 
+            request_body = 
         
         
-        api_url = '{0}{1}'.format(BASE_API_URL,'channels')
-    { "values": [
-{ "componentName": {
-          "value": 22,
-          "raw": 33,
-          "weight": 2,
-          "frequency": "hourly"
+                api_url = '{0}{1}'.format(BASE_API_URL,'channels')
+            { "values": [
+        { "componentName": {
+                "value": 22,
+                "raw": 33,
+                "weight": 2,
+                "frequency": "hourly"
+                }
         }
-}
-], "timestamp":"2020-09-12T10:45:00.23Z"
-}
+        ], "timestamp":"2020-09-12T10:45:00.23Z"
+        }
+        '''
+
+def get_all_device_channels():
+    db = connect_mongo()
+    channel_list = list(db.devices.find({},{'channelID':1,'_id':0}))   
+    return channel_list
 
 
 def get_device_name_for_specified_channel(channel_id):
-    db = db_helpers.connect_mongo()
+    db = connect_mongo()
     result = db.devices.find(
             {'channelID': {'$eq': channel_id}}, {'name': 1, 'channelID': 1})
     if result['name']:
         return {'value':result['name'], 'message':'successfuly found'}
     else:
         return {'value':0, 'message': 'name with channelid not found'}
+
+
+if __name__ == "__main__":
+    results = get_all_device_channels()
+    for channel_id in results:
+        print(channel_id)
+        print('----------')
     
