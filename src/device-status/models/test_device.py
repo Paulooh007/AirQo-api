626f752050432b89d98e32f1f514abe29eaaa52f
lilly-muyama <muyamalillian@gmail.com>
2021-04-20 11:42:39 +0300

    skeleton code

--- src/device-status/models/test_device.py
+++ src/device-status/models/test_device.py
@@ -1,13 +1,21 @@
 import pytest
+import pymongo
+import mongomock
 from unittest.mock import patch 
 from models.device import Device
 
-
+@patch.object(pymongo.MongoClient, 'db_connection.connect_mongo', return_value=mongomock.MongoClient())
 def test_get_devices():
+    device= Device()
     pass
 
+def test_get_device_power():
+    device = Device()
+    pass
 
-def 
+def test_get_device_status():
+    device = Device()
+    pass
 
 if __name__=='__main__':
     pass
