9607d4ed230d6592e067fb4898c2db0418ae9369
lilly-muyama <muyamalillian@gmail.com>
2021-04-20 16:57:37 +0300

    test_convert_GMT_time

--- src/device-status/helpers/test_convert_dates.py
+++ src/device-status/helpers/test_convert_dates.py
@@ -13,17 +13,14 @@ def test_date_to_str():
     assert my_string == '2020-10-23T00:30:58.000000Z'
 
 def test_convert_GMT_time():
-    pass 
+    eat_time = convert_GMT_time_to_EAT_local_time(datetime(2020, 10, 23, 0, 30, 58)) 
+    assert eat_time == 'Fri, 23 Oct 2020 03:30 AM'
 
 def test_convert_to_date():
     pass
 
 if __name__=='__main__':
-    my_date = str_to_date('2020-10-23T00:30:58.000000Z')
-    print(my_date)
-    my_new_date = datetime(2020, 10, 23, 0, 30, 58)
-    print(my_new_date)
-    my_super_date = date_to_str(my_new_date)
-    print(my_super_date)
-
-    #assert my_date == my_new_date
+    gmt_datetime = datetime(2020, 10, 23, 0, 30, 58)
+    eat_datetime = convert_GMT_time_to_EAT_local_time(gmt_datetime)
+    print(eat_datetime)
+    print(type(eat_datetime))
