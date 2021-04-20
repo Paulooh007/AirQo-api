485aeaac095d017d0e8c540cec38ca15f11a1b97
lilly-muyama <muyamalillian@gmail.com>
2021-04-20 11:08:26 +0300

    relative imports

--- src/device-status/models/device.py
+++ src/device-status/models/device.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('./')
 from config import db_connection
 
 
---
+++ src/device-status/models/test_device.py
@@ -0,0 +1,14 @@
+import pytest
+from unittest.mock import patch 
+from models.device import Device
+
+
+def test_get_devices():
+    pass
+
+
+def 
+
+if __name__=='__main__':
+    pass
+
