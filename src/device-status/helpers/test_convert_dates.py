abb0990af7ad23932ccbf8aab367765ddafc628b
lilly-muyama <muyamalillian@gmail.com>
2021-04-20 17:09:04 +0300

    additional tests for str_to_date

--- src/device-status/helpers/test_convert_dates.py
+++ src/device-status/helpers/test_convert_dates.py
@@ -8,9 +8,27 @@ def test_str_to_date():
     my_date = str_to_date('2020-10-23T00:30:58.000000Z')
     assert my_date == datetime(2020, 10, 23, 0, 30, 58)
 
+def test_str_on_empty_arg():
+    with pytest.raises(TypeError):
+        str_to_date()
+
+def test_str_on_incorrect_arg():
+    with pytest.raises(ValueError):
+        str_to_date('Godzilla')
+
+def test_str_on_wrong_arg_type():
+     with pytest.raises(TypeError):
+        str_to_date([1, 2, 3])
+
+def test_str_on_too_many_args():
+     with pytest.raises(TypeError):
+        str_to_date('2020-10-23T00:30:58.000000Z', '2020-10-23T00:30:58.000000Z')
+
 def test_date_to_str():
     my_string = date_to_str(datetime(2020, 10, 23, 0, 30, 58))
     assert my_string == '2020-10-23T00:30:58.000000Z'
+
+def test_date_on_emp
 
 def test_convert_GMT_time():
     eat_time = convert_GMT_time_to_EAT_local_time(datetime(2020, 10, 23, 0, 30, 58)) 
@@ -21,6 +39,4 @@ def test_convert_to_date():
     assert mydate == '2020-10-23'
 
 if __name__=='__main__':
-    mydate = convert_to_date(datetime(2020, 10, 23))
-    print(mydate)
-    print(type(mydate))
+    str_to_date('2020-10-23T00:30:58.000000Z', '2020-10-23T00:30:58.000000Z')
